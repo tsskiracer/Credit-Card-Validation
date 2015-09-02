ff0c87da32bd9dde774b89d2603ec54012d1ad78
tsskiracer <William.Starkey@MU-C1-03.mullen.pvt.k12.co.us>
2015-09-02 15:01:13 -0600

    New Project

---
+++ Credit Card Validation/src/CreditCardVerification.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CreditCardVerification
+	{
+
+		public static void main(String[] args)
+			{
+				Scanner davisIsAJerk = new Scanner(System.in);
+				System.out.println("Please input a 16 digit credit card number.");
+				int myArray[] = new int [16];
+				int cC =davisIsAJerk.nextInt(); 
+				int cCT = davisIsAJerk.nextInt();
+				for (int i = 0; i<myArray.length; i+2)
+					{
+						if (cCT)
+					}
+				for (int j = 1; j<myArray.length; j+2)
+					{
+						
+					}
+
+			}
+
+	}
