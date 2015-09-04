1ef67bb04b5168f24b0ade3d586c6adea6e40088
tsskiracer <William.Starkey@MU-C1-03.mullen.pvt.k12.co.us>
2015-09-04 15:03:26 -0600

    Credit card 2

---
+++ Credit Card Validation/src/CreditCardValidationfromFile.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class CreditCardValidationfromFile
+	{
+
+		public static void main(String[] args) throws IOException
+			{
+				Scanner file = new Scanner(new File("ccnumbers.txt"));
+				System.out.println("Please input a 16 digit credit card number.");
+				file.hasNext();
+				long myArray[] = new long [16];
+				long cC = file.nextLong(); 
+				
+				
+				for (int i = 15; i>=0; i--){
+					if (i%2==0){
+						if((cC%10)>=5){
+							long x=(cC%10)*2;
+							long y=(x/10);
+							long z=(x%10);						
+							myArray[i]=(y+z);}
+						else{
+							myArray[i]=(cC%10)*2;}
+					}
+					else{
+						myArray[i]=cC%10;
+						}
+					cC/=10;
+					}
+				long sum=0;
+				for (long fred:myArray){
+					
+					sum+=fred;
+				}		
+				if (sum%10==0){
+					System.out.println(File + " This card is valid");
+				}
+				else
+					System.out.println(File +" This card is invalid");
+					
+				
+			file.close();
+
+
+			}
+
+	}
--- Credit Card Validation/src/CreditCardVerification.java
+++ Credit Card Validation/src/CreditCardVerification.java
@@ -1,32 +1,37 @@
 import java.util.Scanner;
-public class CreditCardVerification
-	{
+public class CreditCardVerification {
 
-		public static void main(String[] args)
-			{
-				Scanner davisIsAJerk = new Scanner(System.in);
-				System.out.println("Please input a 16 digit credit card number.");
-				long myArray[] = new long [16];
-				long cC = davisIsAJerk.nextInt(); 
-				
-				long x = cC*2;
-				for (int i = 0; i<myArray.length; i+=2)
-					{
-						if (x>9)
-							{
-								myArray[i]=cC%10;
-								cC/=10;
-							}
-					if (sum cC%10 == 0)
-						
-						
-						
-					}
-				for (int j = 1; j<myArray.length; j+=2)
-					{
-						
-					}
-
+	public static void main(String[] args) {
+		Scanner davisIsAJerk = new Scanner(System.in);
+		System.out.println("Please input a 16 digit credit card number.");
+		long myArray[] = new long [16];
+		long cC = davisIsAJerk.nextLong(); 
+		
+		
+		for (int i = 15; i>=0; i--){
+			if (i%2==0){
+				if((cC%10)>=5){
+					long x=(cC%10)*2;
+					long y=(x/10);
+					long z=(x%10);						
+					myArray[i]=(y+z);}
+				else{
+					myArray[i]=(cC%10)*2;}
 			}
-
+			else{
+				myArray[i]=cC%10;
+				}
+			cC/=10;
+			}
+		long sum=0;
+		for (long fred:myArray){
+			
+			sum+=fred;
+		}		
+		if (sum%10==0){
+			System.out.println("The card is valid.");}
+		else{
+			System.out.println("The card is invalid");}
 	}
+
+}
